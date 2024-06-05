a6a3a7f429a8ef914e20765b3ed40e62a1b01270
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-06-05 12:20:51 +0530

    init: Integrate Express middleware with Apollo Server and connect MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,39 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import dotenv from "dotenv";
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
+
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
+
+const app = express();
+const httpServer = http.createServer(app);
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
   resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server);
+await server.start();
 
-console.log(`🚀 Server ready at ${url}`);
+app.use(
+  "/",
+  cors(),
+  express.json(),
+
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req }),
+  })
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB();
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -5,6 +5,9 @@ const userResolver = {
     users: () => {
       return users;
     },
+    user: (_, { userId }) => {
+      return users.find((user) => user._id === userId);
+    },
   },
   Mutation: {},
 };
