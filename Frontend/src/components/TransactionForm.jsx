4729e388a91a94bc1594ebd8051d852082def326
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-06-07 12:52:34 +0530

    feat: CRUD Operation For Transactions

--- Frontend/src/components/TransactionForm.jsx
+++ Frontend/src/components/TransactionForm.jsx
@@ -1,4 +1,12 @@
+import { useMutation } from "@apollo/client";
+import { CREATE_TRANSACTION } from "../graphql/mutations/transcation.mutation";
+import toast from "react-hot-toast";
+
 const TransactionForm = () => {
+  const [createTransaction, { loading }] = useMutation(CREATE_TRANSACTION, {
+    refetchQueries: ["GetTransactions"],
+  });
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -12,7 +20,14 @@ const TransactionForm = () => {
       location: formData.get("location"),
       date: formData.get("date"),
     };
-    console.log("transactionData", transactionData);
+    try {
+      await createTransaction({ variables: { input: transactionData } });
+
+      form.reset();
+      toast.success("Transaction created successfully");
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   return (
@@ -159,8 +174,9 @@ const TransactionForm = () => {
           from-pink-500 to-pink-500 hover:from-pink-600 hover:to-pink-600
 						disabled:opacity-70 disabled:cursor-not-allowed"
         type="submit"
+        disabled={loading}
       >
-        Add Transaction
+        {loading ? "Loading..." : "Add Transaction"}
       </button>
     </form>
   );
